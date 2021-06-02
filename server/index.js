077400210defb70d5122f5ad8c48ce544ae5e71f
g3raphacs <g3raphacs@gmail.com>
2021-06-02 11:15:41 +0200

    user + send message

--- server/index.js
+++ server/index.js
@@ -30,6 +30,16 @@ io.on('connection', (socket)=>{
         console.log(`${user.name} has joined!`)
 
         socket.join(user.room)
+
+        callback()
+    })
+
+    socket.on('sendMessage' , (message , callback) => {
+      const user = getUser(socket.id)
+
+      io.to(user.room).emit('message' , { user: user.name, text: message})
+
+      callback()
     })
 
     socket.on('disconnect', ()=>{
